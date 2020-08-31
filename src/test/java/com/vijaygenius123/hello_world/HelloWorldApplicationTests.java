1cf59d785a7b4431e819ef9d245d13ac4dd2eec9
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-31 22:02:11 +0530

    Initial commit

---
+++ src/test/java/com/vijaygenius123/hello_world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
